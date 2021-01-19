6098da4544c55a8e80adbe86eed50455ad75baeb
alvaro guajardo <aguajardr@gmail.com>
2021-01-19 14:34:10 -0300

    primera subida

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Header from './component/header/header'
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/header/header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <div className="bg-black">
+            <div>
+                <div></div>
+                <div>
+                    <di></di>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default header
