e8a81dc759e6e44f1263e4cc797f51dc642e62a4
alvaro guajardo <aguajardr@gmail.com>
2021-02-01 19:43:42 -0300

    Pagina version 1.1

--- src/component/body/Cotizacion.js
+++ src/component/body/Cotizacion.js
@@ -11,100 +11,120 @@ const Cotizacion = () => {
           </h1>
         </div>
       </div>
-      <div className="">
-        <div className="grid justify-items-center">
-          <img src={auto} alt="Peugeot" className="w-72 pt-8"></img>
+      <div className="md:w-1/2 md:float-left md:pt-64 md:mr-5">
+        <div className="grid justify-items-center md:place-content-center">
+          <img src={auto} alt="Peugeot" className="w-72 pt-8 md:w-96"></img>
         </div>
       </div>
-      <div className="pt-7">
-        <form className="grid grid-cols-3">
-          <div className="grid justify-items-center col-span-3">
+      <div className="pt-7 md:w-full">
+        <form className="grid grid-cols-5">
+          <div className="grid justify-items-center md:justify-items-start col-span-5">
             DATOS DE CONTACTO
           </div>
-          <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="first-name"
-          ></label>
-          <input
-            className="col-span-3 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
-            id="Name"
-            type="text"
-            placeholder="Nombre Completo"
-          />
-          <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="EMail"
-          ></label>
-          <input
-            className="col-span-3 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
-            id="email"
-            type="email"
-            placeholder="E-Mail"
-          />
-          <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="Telefono"
-          ></label>
-          <input
-            className="col-span-3 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
-            id="telefono"
-            type="tel"
-            placeholder="Telefono"
-          />
-          <div className="grid justify-items-center col-span-3 pt-6">
+          <div className="col-span-5 lg:col-span-4">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="first-name"
+            ></label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
+              id="Name"
+              type="text"
+              placeholder="Nombre Completo"
+            />
+          </div>
+          <div className="col-span-5 lg:col-span-4">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="EMail"
+            ></label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
+              id="email"
+              type="email"
+              placeholder="E-Mail"
+            />
+          </div>
+          <div className="col-span-5 lg:col-span-4">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="Telefono"
+            ></label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
+              id="telefono"
+              type="tel"
+              placeholder="Telefono"
+            />
+          </div>
+          <div className="grid justify-items-center col-span-5 pt-6 md:justify-items-start">
             DATOS DE LA CARGA
           </div>
-          <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="Ciudad1"
-          ></label>
-          <input
-            className="col-span-3 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
-            id="ciudad1"
-            type="text"
-            placeholder="Ciudad de origen"
-          />
-          <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="Ciudad2"
-          ></label>
-          <input
-            className="col-span-3 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
-            id="ciudad2"
-            type="text"
-            placeholder="Ciudad de destino"
-          />
-          <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="Peso"
-          ></label>
-          <input
-            className="col-span-3 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
-            id="peso"
-            type="text"
-            placeholder="Peso (Kg)"
-          />
-          <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="dimencion"
-          ></label>
-          <input
-            className="col-span-3 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
-            id="dimencion"
-            type="text"
-            placeholder="Dimenciones (Largo x Ancho x Alto)"
-          />
-          <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="comentarios"
-          ></label>
-          <textarea
-            className="col-span-3 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
-            id="message"
-            type="textarea"
-            rows="3"
-            placeholder="Comentarios Adicionales"
-          />
+          <div className="col-span-5 lg:col-span-2 md:justify-items-auto lg:mr-2">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="Ciudad1"
+            ></label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 
+            mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde 
+            focus:border-transparent"
+              id="ciudad1"
+              type="text"
+              placeholder="Ciudad de origen"
+            />
+          </div>
+          <div className="col-span-5 lg:col-span-2">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="Ciudad2"
+            ></label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 
+            mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde 
+            focus:border-transparent"
+              id="ciudad2"
+              type="text"
+              placeholder="Ciudad de destino"
+            />
+          </div>
+          <div className="col-span-5">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="Peso"
+            ></label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
+              id="peso"
+              type="text"
+              placeholder="Peso (Kg)"
+            />
+          </div>
+          <div className="col-span-5">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="dimencion"
+            ></label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
+              id="dimencion"
+              type="text"
+              placeholder="Dimenciones (Largo x Ancho x Alto)"
+            />
+          </div>
+          <div className="col-span-5">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="comentarios"
+            ></label>
+            <textarea
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-verde focus:border-transparent"
+              id="message"
+              type="textarea"
+              rows="3"
+              placeholder="Comentarios Adicionales"
+            />
+          </div>
         </form>
       </div>
     </div>
